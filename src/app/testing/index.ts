3bc5cda923c4b2db41c5e119a0136df66a5d5593
Roman Viznij <starsetphenomenon@gmail.com>
2026-02-17 10:29:42 +0200

    jasmine removed

--- src/app/testing/index.ts
+++
@@ -1,8 +0,0 @@
-export { TestUtils } from './test-utils';
-export {
-  createFactServiceMock,
-  createNotificationServiceMock,
-  type FactServiceMock,
-  type NotificationServiceMock,
-  type FactServiceMockOptions,
-} from './mocks';
